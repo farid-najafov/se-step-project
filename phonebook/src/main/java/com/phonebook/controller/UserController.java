df3a7893307abd98d0ef6b9d87a245aaa08769a8
farid-najafov <farid.najafov@outlook.com>
2021-12-05 22:00:42 +0400

    created user data file

--- phonebook/src/main/java/com/phonebook/controller/UserController.java
+++ phonebook/src/main/java/com/phonebook/controller/UserController.java
@@ -8,18 +8,12 @@ import java.util.List;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("user")
+@CrossOrigin("*")
 public class UserController {
 
     private final UserService userService;
--- terraform/be/user_data.sh
+++ terraform/be/user_data.sh
@@ -5,4 +5,11 @@ yum install docker -y
 usermod -aG docker ec2-user
 systemctl enable docker
 systemctl start docker
+yum install git -y
 
+git clone https://github.com/farid-najafov/se-step-project.git
+
+cd se-step-project/phonebook
+
+docker build -t phonebook-app:v1.0.0 .
+docker run --name phonebook-app -p 80:80 phonebook-app:v1.0.0
